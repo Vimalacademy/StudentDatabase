3ca750d92df97fb36077763ac5fada68ca84ecf3
CNN-COMPUTERS <CNN-COMPUTERS@DESKTOP-O53GGJV>
2023-10-05 16:24:31 +0530

    Add project files.

---
+++ StudentDatabase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentDbContext())
+            {
+
+                // Create a new student
+                var newStudent = new Student
+                {
+                    Name = "John Doe",
+                    EnrollmentDate = DateTime.Now
+                };
+
+                // Add the student to the database
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+
+                Console.WriteLine("Student added to the database.");
+            }
+        }
+    }
+}
+
+
+
---
+++ StudentDatabase/StudentDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace StudentDatabase
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
